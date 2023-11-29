3dc374acc83b23f38af8dd6362fce4e807aff95d
Raffy Alcoriza <jrpalcoriza@outlook.com>
2023-11-29 15:21:51 +0800

    Properly set up env base URL in frontend

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ DB_USER="pguser"
 DB_NAME="referral_db"
 DB_PASSWORD=$(openssl rand -base64 32 | tr -d '=+/' | cut -c -32)
 
+API_BASE_URL="http://localhost:3000"
+
 # Prefill postgres values to .env
 cat << EOF > .env
 POSTGRES_HOST="$DB_HOST"
@@ -21,7 +23,12 @@ DATABASE_URL="postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 EOF
 
 # Prefill postgres values to packages/prisma/.env
-cat << EOF > packages/prisma/.env
+cat << EOF > packages/db-prisma/.env
 DATABASE_URL="postgresql://$DB_USER:$DB_PASSWORD@$DB_HOST:$DB_PORT/$DB_NAME"
 EOF
 
+# Prefill postgres values to packages/frontend/.env.local
+cat << EOF > packages/frontend/.env.local
+API_BASE_URL="$API_BASE_URL"
+NEXT_PUBLIC_API_BASE_URL="$API_BASE_URL"
+EOF
