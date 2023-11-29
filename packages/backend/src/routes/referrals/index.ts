5b1d0d21639397789fa986194a5ae15cffc7a4c9
Raffy Alcoriza <jrpalcoriza@outlook.com>
2023-11-29 17:11:35 +0800

    Enable PUT endpoint for referrals

--- packages/backend/src/routes/referrals/index.ts
+++ packages/backend/src/routes/referrals/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import DELETE_REFERRAL from "./:id/delete";
 import GET_REFERRAL from "./:id/get";
+import PUT_REFERRAL from "./:id/put";
 import GET from "./get";
 import POST from "./post";
 
@@ -11,6 +12,7 @@ router.get("/", GET);
 router.post("/", POST);
 
 router.get("/:id", GET_REFERRAL);
+router.put("/:id", PUT_REFERRAL);
 router.delete("/:id", DELETE_REFERRAL);
 
 export default router;
